f41a3452974a8c519de6273e78a650c27b36f360
Joshua Bell <joshua@rageon.com>
2016-12-08 16:06:27 -0800

    added push cert and updated code for push

--- index.js
+++ index.js
@@ -18,7 +18,15 @@ var api = new ParseServer({
     appId: process.env.APP_ID || 'myAppId',
     masterKey: process.env.MASTER_KEY || '', //Add your master key here. Keep it secret!
     serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
-    allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false
+    allowClientClassCreation: process.env.CLIENT_CLASS_CREATION || false,
+    push: {
+        ios: {
+            pfx: '/certs/RageOn-Push-Services.p12',
+            passphrase: process.env.PUSH_CERT_PASSWORD, // optional password to your p12/PFX
+            bundleId: 'com.rageon.rageon',
+            production: true
+        }
+    }
 
 });
 
